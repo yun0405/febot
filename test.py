18153ad8d5badf3e65f3a263b5290f30bbac98ed
yun0405 <98749859+yun0405@users.noreply.github.com>
2022-10-13 08:12:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 import random
+from dotenv import load_dotenv
 import os
+import replicate
+load_dotenv()
 bot = commands.Bot(command_prefix='!')
 
 @bot.event
@@ -178,7 +181,16 @@ async def 명령어(ctx):
 @bot.command()
 async def 로또(ctx):
     await ctx.send(f'로또 예상 번호는{random.sample(range(1,46),6)}')
-      
+    
+@bot.command(aliases=["sd"])
+async def 그려줘(ctx, *, prompt):
+    """Generate an image from a text prompt using the stable-diffusion model"""
+    msg = await ctx.send(f"“{prompt}”\n> Generating...")
+
+    model = replicate.models.get("stability-ai/stable-diffusion")
+    image = model.predict(prompt=prompt)[0]
+
+    await msg.edit(content=f"“{prompt}”\n{image}")      
     
 access_token = os.environ['BOT_TOKEN']
 bot.run(access_token)
